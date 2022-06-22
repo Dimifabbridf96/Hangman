9860e7fea41dce456f9783f87d3f8521a916fa8a
Dimifabbridf96 <dimi.fabbri@gmail.com>
2022-06-22 18:31:00 +0000

    start project

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from word import WORDS
+
+print('Welcome to the game','''+---+
+                    |   |
+                        |
+                        |
+                        |
+                        |
+                    =========''')
+
+
+word = random.choice(WORDS)
+guess = "_" * len(word)
+print(guess)
+
