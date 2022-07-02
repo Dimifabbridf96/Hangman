0a58d3b0ac0e37b571bc98cc92cfd65c3e3f128d
Dimifabbridf96 <dimi.fabbri@gmail.com>
2022-07-02 20:05:40 +0000

    rid off of restart function

--- run.py
+++ run.py
@@ -24,21 +24,22 @@ class User:
         print( f"Ready to guess {self.name} ? ")     
 user = User(user_name)
 
-word = random.choice(WORDS)
-guess = "_" * len(word)
-guess_word = set(word)
-letters_in_word = set()
-letters_used = set()
-print(f"{guess} \n")
 
 
-def check_letter_in_word(): 
+
+def check_letter_in_word():
+    word = random.choice(WORDS)
+    guess = "_" * len(word)
+    guess_word = set(word)
+    letters_in_word = set()
+    letters_used = set()
+    print(f"{guess} \n")
     global lives
     global coins
     
     while guess_word != letters_in_word:  
        
-        user_choice = input("Guess a word: ").upper()
+        user_choice = input("Guess a word:").upper()
         if len(user_choice) > 1:
             print("Invalid input try again")
             user_choice = input("Guess a word: ").upper()
@@ -48,21 +49,75 @@ def check_letter_in_word():
             print('letter already used')
             continue
         x = letters_used.add(user_choice)
-        print(letters_used)
+        
         if word.find(user_choice) == -1:
             print("I guess that you didn't guess 🤣")
             lives = lives - 1
-            print(lives)
-            print(letters_used)
-            print(coins)
-        
+            print(f"{lives} lives remained")
+            print(f"You used already {letters_used} ")
+            print(f"{coins} coin/s")
         else:
             lives = lives + 1
             coins = coins + 5
-            print("i guess you guess ✌")
-            print(lives)
-            print(letters_used)
-            print(coins)
+            print("i guess you guess ✌\n")
+            print(f"{lives} lives remained\n")
+            print(f"You used already {letters_used}\n")
+            print(f"{coins} coin/s\n")
+                
+            if user_choice in word:
+                letters_in_word.add(user_choice)
+                print(f"{letters_in_word} are present in the word")
+        current_word = ""
+        for letter in range(len(word)):
+           
+            if user_choice == word[letter]:
+                current_word += user_choice
+            else:
+                current_word += guess[letter]
+
+        guess = current_word
+        print(guess)
+    replay()
+
+"""def restart(): 
+    
+    word = random.choice(WORDS)
+    guess_word = set(word)
+    letters_in_word = set()
+    letters_used = set()
+    guess_word = set(word)
+    guess = "_" * len(word)
+    print(f"{guess} \n")
+  
+    global lives
+    global coins
+    
+    while guess_word != letters_in_word:  
+       
+        user_choice = input("Guess a word:").upper()
+        if len(user_choice) > 1:
+            print("Invalid input try again")
+            user_choice = input("Guess a word: ").upper()
+        else:
+            pass
+        if user_choice in letters_used:
+            print('letter already used')
+            continue
+        x = letters_used.add(user_choice)
+        
+        if word.find(user_choice) == -1:
+            print("I guess that you didn't guess 🤣")
+            lives = lives - 1
+            print(f"{lives} lives remained")
+            print(f"You used already {letters_used} ")
+            print(f"{coins} coin/s")
+        else:
+            lives = lives + 1
+            coins = coins + 5
+            print("i guess you guess ✌\n")
+            print(f"{lives} lives remained\n")
+            print(f"You used already {letters_used}\n")
+            print(f"{coins} coin/s\n")
                 
             if user_choice in word:
                 letters_in_word.add(user_choice)
@@ -78,63 +133,13 @@ def check_letter_in_word():
         guess = current_word
         print(guess)
 
-def restart(): 
-    global lives
-    word = random.choice(WORDS)
-    guess_word = set(word)
-    letters_in_word = set()
-    letters_used = set()
-    guess_word = set(word)
-    guess = "_" * len(word)
-    print(f"{guess} \n")
-  
-    while guess_word != letters_in_word:  
-       
-        user_choice = input("Guess a word: ").upper()
-        if len(user_choice) > 1:
-            print("Invalid input try again")
-            user_choice = input("Guess a word: ").upper()
-        else:
-            pass
-        if user_choice in letters_used:
-            print('letter already used')
-            continue
-        x = letters_used.add(user_choice)
-        print(letters_used)
-        if word.find(user_choice) == -1:
-            print("I guess that you didn't guess 🤣")
-            lives = lives - 1
-            print(lives)
-            print(letters_used)
-        
-        else:
-            lives = lives + 1
-            print("i guess you guess ✌")
-            print(lives)
-            print(letters_used)
-                
-            if user_choice in word:
-                letters_in_word.add(user_choice)
-                print(f"{letters_in_word} are present in the word")
-
-        current_word = ""
-        
-        for letter in range(len(word)):
-           
-            if user_choice == word[letter] :
-                current_word += user_choice
-            else:
-                current_word += guess[letter]
-
-        guess = current_word
-        print(guess)
-    replay()
-
+    
+"""
 def replay():
     global WORDS
     play = input("Do you want to do another game y/n ??").upper()
     if play == "Y":
-        restart()
+        check_letter_in_word()
     elif play == "N":
         print("Thanks for your time")
     else:
